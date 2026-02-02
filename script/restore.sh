a1283537ff358147dd8d2d6cd9fc050429dc7638
koranit <koranit.17k@gmail.com>
2026-02-02 16:12:10 +0700

    upgrade table

--- script/restore.sh
+++ script/restore.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-unzip payroll.zip 
+unzip /home/koranit/Downloads/payroll.zip 
 sudo mysql payroll < payroll.sql
-
+rm payroll.sql
+sudo mysql payroll < /home/koranit/attendance/sql/view.sql
+sudo mysql payroll < /home/koranit/attendance/sql/myview.sql
 
